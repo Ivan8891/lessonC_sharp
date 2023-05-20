c6dde533f6c05d586b2b7169146faa68aa24aacf
Ivan <crueldog50@gmail.com>
2023-05-20 14:51:25 +1000

    task2 finish

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,3 +2,16 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+Console.WriteLine("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (max < b)
+    max = b;
+if (max < c)
+    max = c;
+Console.WriteLine("Число " + max + " самое большое.");
